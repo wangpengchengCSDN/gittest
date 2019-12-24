f1c09366149d6995c84a8fe6dc6b664f7e0ca641
wpengc <wangpc@yinhechuangxiang.com>
2019-12-24 14:25:26 +0800

    develop01 提交01

--- src/main/java/com/yaic/Develop01.java
+++ src/main/java/com/yaic/Develop01.java
@@ -3,6 +3,6 @@ package com.yaic;
 public class Develop01 {
 
     public static void main(String[] args) {
-        System.out.println("develop 分支 创建第一个Class");
+        System.out.println("develop 分支 创建第一个Class ");
     }
 }
