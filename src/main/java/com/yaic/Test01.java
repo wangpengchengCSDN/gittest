86bda825be593eb29779ae8ba62507f1a4a16717
wpengc <wangpc@yinhechuangxiang.com>
2019-12-24 15:35:14 +0800

    develop  修改master 同一个文件代码提交  01

--- src/main/java/com/yaic/Test01.java
+++ src/main/java/com/yaic/Test01.java
@@ -4,5 +4,7 @@ public class Test01 {
 
     public static void main(String[] args) {
         System.out.println("master首次提交 01");
+
+        System.out.println("develop 分支 修改了master 分支代码 01");
     }
 }
