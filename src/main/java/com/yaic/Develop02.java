acc0d1459daddf6807530836159cdab925b7ee3d
wpengc <wangpc@yinhechuangxiang.com>
2019-12-25 09:35:27 +0800

    develop 分支新建develop02类 提交01

---
+++ src/main/java/com/yaic/Develop02.java
@@ -0,0 +1,10 @@
+package com.yaic;
+
+public class Develop02 {
+
+    public static void main(String[] args) {
+        for (int i=0;i<20;i++){
+            System.out.println("congnan dao bei");
+        }
+    }
+}
